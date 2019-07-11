f320090d846ea306783bba98d15778cc6b8c3b1e
Andrew Bay <daddnbay@yahoo.com>
2019-07-11 19:47:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,18 +1,6 @@
 # code here!
 class School
 
-  def roster
-    roster = []
-  end
-  
-  def initialize(school)
-    @school = school
-  end
-  
-  def add_student
-   add_student["name"] = []
-   add_student["name"] << "grade"
-  end
   
   
 end  
